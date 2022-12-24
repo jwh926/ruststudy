0e5b957c2f3987eb556bf14bddd05c76526535ae
wono <tridath3@gmail.com>
2022-12-25 00:48:15 +0900

    convert degrees F/C to degrees C/F

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,18 @@
+use std::io;
+
 fn main() {
-	for i in 1..10 {
-		println!("{}", i);
+	let mut temp = String::new();
+	io::stdin().read_line(&mut temp).unwrap();
+	let mut temp = temp.trim().split_whitespace();
+	let temperature:f64 = temp.next().unwrap().parse().unwrap();
+	let method = temp.next().unwrap();
+	if method == "F" {
+		println!("{} C", (temperature-32.0)/1.8);
+	}
+	else if method == "C" {
+		println!("{} F", (temperature*1.8)+32.0);
+	}
+	else {
+		println!("method is wrong: F or C");
 	}
 }
